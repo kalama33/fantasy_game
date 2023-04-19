96efbc5b9c4a519c0d84440912f50882c59a68b5
kalama <128490513+kalama33@users.noreply.github.com>
2023-04-20 00:42:57 +0200

    creator
    creator.py and readme

---
+++ creator.py
@@ -0,0 +1,185 @@
+print( "Welcome to the character generator!")
+
+print()
+
+print("Please, choose five names; ")
+
+print()
+
+name_1 = input("1st character: ")
+name_2 = input("2nd character: ")
+name_3 = input("3rd character: ")
+name_4 = input("4th character: ")
+name_5 = input("5th character: ")
+
+print()
+
+print("These are your characters!")
+
+import random
+
+tribe_1= random.randint(0,3)
+tribe_2= random.randint(0,3)
+tribe_3= random.randint(0,3)
+tribe_4= random.randint(0,3)
+tribe_5= random.randint(0,3)
+
+if tribe_1 == 1:
+    print(name_1, "is a Warrior!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght*3)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+if tribe_1== 2:
+    print(name_1, "is a Wizard!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic*3)
+
+
+if tribe_1 == 3:
+    print(name_1, "is a Potato!")
+    Health = random.randint(5,10)
+    print("Health:",Health*3)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+print()
+
+if tribe_2 == 1:
+    print(name_2, "is a Warrior!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght*3)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+if tribe_2 == 2:
+    print(name_2, "is a Wizard!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic*3)
+
+
+if tribe_2 ==3:
+    print(name_2, "is a Potato!")
+    Health = random.randint(5,10)
+    print("Health:",Health*3)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+print()
+
+if tribe_3 == 1:
+    print(name_3, "is a Warrior!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght*3)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+if tribe_3 == 2:
+    print(name_3, "is a Wizard!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic*3)
+
+
+if tribe_3 ==3:
+    print(name_3, "is a Potato!")
+    Health = random.randint(5,10)
+    print("Health:",Health*3)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+print()
+
+if tribe_4 == 1:
+    print(name_4, "is a Warrior!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght*3)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+if tribe_4 == 2:
+    print(name_4, "is a Wizard!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic*3)
+
+
+if tribe_4 == 3:
+    print(name_4, "is a Potato!")
+    Health = random.randint(5,10)
+    print("Health:",Health*3)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+print()
+
+if tribe_5 == 1:
+    print(name_5, "is a Warrior!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght*3)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+
+if tribe_5 == 2:
+    print(name_5, "is a Wizard!")
+    Health = random.randint(5,10)
+    print("Health:",Health)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic*3)
+
+
+if tribe_5 == 3:
+    print(name_5, "is a Potato!")
+    Health = random.randint(5,10)
+    print("Health:",Health*3)
+    Strenght = random.randint(5,10)
+    print("Strength:",Strenght)
+    Magic = random.randint(5,10)
+    print("Magic:",Magic)
+    
+print()
+
+print("Your brave team!", name_1, name_2, name_3, name_4, name_5)
+
+print("Happy adventure!")
+      
+        
+
+
+
+    
